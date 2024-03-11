51dbd2a9ccb578fe9a8045539e6cc57e3f4028ec
AnjaHrydziushka <anja.hryd@gmail.com>
2024-03-11 16:44:11 +0100

    Redux learning

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
+import { deposit, withdraw, reset } from "./store/balance/slice";
+import { selectBalance } from "./store/balance/selectors";
 
 function App() {
+  const dispatch = useDispatch();
+  const balance = useSelector(selectBalance);
+
+  const [amount, setAmount] = useState(0);
+
+  function addAmount(e) {
+    dispatch(deposit(amount));
+    setAmount(0);
+    e.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Balance: {balance}$</p>
+      <button
+        onClick={() => {
+          dispatch(deposit(10));
+        }}
+      >
+        Deposit 10$
+      </button>{" "}
+      <button
+        onClick={() => {
+          dispatch(withdraw(10));
+        }}
+      >
+        Withdraw 10$
+      </button>{" "}
+      <button
+        onClick={() => {
+          dispatch(reset());
+        }}
+      >
+        Reset
+      </button>{" "}
+      <input
+        type="number"
+        placeholder="Amount"
+        value={amount}
+        onChange={(e) => setAmount(parseInt(e.target.value))}
+      />{" "}
+      <button onClick={addAmount}>Add money</button>
     </div>
   );
 }
